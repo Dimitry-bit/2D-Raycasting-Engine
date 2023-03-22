535b34448ca7b2725a00c31935153feeb836582d
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2023-03-22 17:02:44 +0200

    Add: ray Create/LookAt/Cast/Draw

---
+++ include/ray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+
+#include "boundary.h"
+
+struct ray_t {
+	sf::Vector2f origin;
+	sf::Vector2f direction;
+};
+
+ray_t CreateRay(float x1, float y1, float x2, float y2);
+ray_t CreateRay(float x1, float y1, float angleInDegree);
+ray_t CreateRay(sf::Vector2f origin, sf::Vector2f direction);
+ray_t CreateRay(sf::Vector2f origin, float angleInDegree);
+
+void RayLookAt(ray_t& ray, const sf::Vector2f& target);
+bool RayCast(const ray_t& ray, const boundary_t& boundary, sf::Vector2f& point);
+
+void DrawRay(const ray_t& ray);
---
+++ src/ray.cpp
@@ -0,0 +1,112 @@
+#include <cmath>
+
+#include "ray.h"
+#include "renderer.h"
+#include "boundary.h"
+
+void Vector2fNormalize(sf::Vector2f& vec2f);
+sf::Vector2f Vector2fFromAngle(float len, float angleInDegree);
+
+ray_t CreateRay(float x1, float y1, float x2, float y2)
+{
+	sf::Vector2f dir(x2, y2);
+	Vector2fNormalize(dir);
+
+	return ray_t{
+		{x1, y1},
+		dir,
+	};
+}
+
+ray_t CreateRay(sf::Vector2f origin, sf::Vector2f direction)
+{
+	Vector2fNormalize(direction);
+	return ray_t{
+		origin,
+		direction,
+	};
+}
+
+ray_t CreateRay(float x1, float y1, float angleInDegree)
+{
+	sf::Vector2f dir = Vector2fFromAngle(1.0f, angleInDegree);
+	return ray_t{
+		{x1, y1},
+		dir,
+	};
+}
+
+ray_t CreateRay(sf::Vector2f origin, float angleInDegree)
+{
+	sf::Vector2f dir = Vector2fFromAngle(1.0f, angleInDegree);
+	return ray_t{
+		origin,
+		dir,
+	};
+}
+
+void RayLookAt(ray_t& ray, const sf::Vector2f& target)
+{
+	ray.direction.x = target.x - ray.origin.x;
+	ray.direction.y = target.y - ray.origin.y;
+	Vector2fNormalize(ray.direction);
+}
+
+bool RayCast(const ray_t& ray, const boundary_t& boundary, sf::Vector2f& point)
+{
+	const float x1 = boundary.pA.x;
+	const float y1 = boundary.pA.y;
+	const float x2 = boundary.pB.x;
+	const float y2 = boundary.pB.y;
+
+	const float x3 = ray.origin.x;
+	const float y3 = ray.origin.y;
+	const float x4 = ray.origin.x + ray.direction.x;
+	const float y4 = ray.origin.y + ray.direction.y;
+
+	const float den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+	if (den == 0)
+		return false;
+
+	float t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+	float u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+
+	if ((t > 0) && (t < 1) && (u > 0)) {
+		point.x = x1 + t * (x2 - x1);
+		point.y = y1 + t * (y2 - y1);
+		return true;
+	}
+
+	return false;
+}
+
+void DrawRay(const ray_t& ray)
+{
+	sf::Vertex line[] = {
+		sf::Vertex(ray.origin),
+		sf::Vertex(ray.origin + (ray.direction * 50.0f))
+	};
+
+	rWindow.draw(line, 2, sf::Lines);
+}
+
+void Vector2fNormalize(sf::Vector2f& vec2f)
+{
+	const float mag = sqrt((vec2f.x * vec2f.x) + (vec2f.y * vec2f.y));
+
+	if (mag <= 0)
+		return;
+
+	vec2f.x /= mag;
+	vec2f.y /= mag;
+}
+
+sf::Vector2f Vector2fFromAngle(float len, float angleInDegree)
+{
+	const double angleInRad = angleInDegree * std::numbers::pi / 180;
+	float x = len * cos(angleInRad);
+	float y = len * sin(angleInRad);
+
+	return sf::Vector2f{x, y};
+}
