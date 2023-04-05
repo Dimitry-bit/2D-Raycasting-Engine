205422d4437b5037ade8da36226c15811006a890
Dimitry <73913361+Dimitry-bit@users.noreply.github.com>
2023-04-05 21:54:40 +0200

    Refactor: inline defaultColorpalette

--- include/engine_color.h
+++ include/engine_color.h
@@ -12,4 +12,12 @@ struct pallet_t {
 	sf::Color selection;
 };
 
-extern pallet_t defaultColorPalette;
+inline pallet_t defaultColorPalette = {
+	.background = sf::Color::Black,
+	.boundary = sf::Color::White,
+	.point = sf::Color::White,
+	.particle = sf::Color::White,
+	.ray = sf::Color(255, 255, 255, 70),
+	.hitRay = sf::Color(255, 255, 255, 70),
+	.selection = sf::Color::Cyan,
+};
